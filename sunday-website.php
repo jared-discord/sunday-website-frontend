c7202a2647cc620ac05f76ce4d222fce2764d990
jared <electrojunglefck@gmail.com>
2019-08-28 13:25:04 -0400

    python test

--- sunday-website.php
+++ sunday-website.php
@@ -6,6 +6,11 @@ album: <?php echo $_POST["album"]; ?><br>
 year: <?php echo $_POST["year"]; ?><br>
 file type: <?php echo $_POST["filetype"]; ?><br>
 
+<?php 
+    $command = escapeshellcmd('hello.py');
+    $output = shell_exec($command);
+    echo $output;
+?>
 
 </body>
 </html>
