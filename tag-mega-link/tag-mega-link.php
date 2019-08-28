a2c68d6d846fa5a4952a2115f3e6f0f6db040940
jared <electrojunglefck@gmail.com>
2019-08-28 16:12:58 -0400

    added more fields

--- tag-mega-link/tag-mega-link.php
+++ tag-mega-link/tag-mega-link.php
@@ -5,6 +5,7 @@ artist: <?php echo $_POST["artist"]; ?><br>
 album: <?php echo $_POST["album"]; ?><br>
 year: <?php echo $_POST["year"]; ?><br>
 file type: <?php echo $_POST["filetype"]; ?><br>
+description: <?php echo $_POST["description"]; ?><br>
 
 results: <?php 
     $command = escapeshellcmd('./tag-mega-link.py');
