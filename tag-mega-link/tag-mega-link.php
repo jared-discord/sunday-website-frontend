c8810e1a12996a600be3151f4717d7d53ee89efb
jared-discord <54513845+jared-discord@users.noreply.github.com>
2019-08-28 21:55:11 -0400

    Mostly finished. Now i have to set up a flask server to commuicate with bot

--- tag-mega-link/tag-mega-link.php
+++ tag-mega-link/tag-mega-link.php
@@ -1,15 +1,11 @@
 <html>
 <body>
 
-artist: <?php echo $_POST["artist"]; ?><br>
-album: <?php echo $_POST["album"]; ?><br>
-year: <?php echo $_POST["year"]; ?><br>
-file type: <?php echo $_POST["filetype"]; ?><br>
-genre: <?php echo $_POST["genre"]; ?><br>
-description: <?php echo $_POST["description"]; ?><br>
-
-results: <?php 
-    $pythonCmd = "./tag-mega-link.py " . $_POST["artist"] . " " . $_POST["album"] . " " . $_POST["filetype"] . " " . $_POST["year"] . " " . $_POST["genre"] . " " . $_POST["description"];
+<?php
+$pythonCmd = "./tag-mega-link.py";
+    foreach ($_POST as $key => $value) { 
+        $pythonCmd.= " " . htmlspecialchars($key) . " " . htmlspecialchars($value);
+    }
     $command = escapeshellcmd($pythonCmd);
     $output = shell_exec($command);
     echo "$output";
--- tag-mega-link/tag-mega-link.py
+++ tag-mega-link/tag-mega-link.py
@@ -1,5 +1,18 @@
 #!/usr/bin/env python
 
+#this python script recieves data to POST to the discord bot via command line arugments
+#format of command line arugments is "tag-mega-link.py keyOne valueOne keyTwo valueTwo... "
 import sys
+import fileinput 
+import requests
 
-print (str(sys.argv))
+sys.argv.pop(0) #pop filename at head of argv list
+postData = {} #data which we will send in our post request to the discrod bot
+i = 0
+while(i < len(sys.argv)-1): #build the post data dictionary from the command line arugments 
+    postData[sys.argv[i]] = sys.argv[i+1]
+    i += 2
+
+r = requests.post(url = "127.0.0.1:PORTNUMBER", data = postData)
+response = r.text
+print(response)
