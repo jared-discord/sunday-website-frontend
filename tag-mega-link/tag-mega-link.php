ede8b756b131188d27b4dde05748ab71dba028e1
jared-discord <54513845+jared-discord@users.noreply.github.com>
2019-09-24 19:23:40 -0400

    Update tag-mega-link.php

--- tag-mega-link/tag-mega-link.php
+++ tag-mega-link/tag-mega-link.php
@@ -7,6 +7,7 @@ $pythonCmd = "./tag-mega-link.py";
     foreach ($_POST as $key => $value) { 
         $pythonCmd.= " \"" . htmlspecialchars($key) . "\" \"" . htmlspecialchars($value) . "\"";
     }
+    echo "$pythonCmd";
     $command = escapeshellcmd($pythonCmd);
     $output = shell_exec($command);
     echo "$output";
